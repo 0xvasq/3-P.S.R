2eafdba6837416c1c60db7f6a8ff37ae7fe1b4dd
0xvasq <jorge.vasquez007@mymdc.net>
2024-04-16 14:59:38 -0400

    Add counter case 2

--- Source1.cpp
+++ Source1.cpp
@@ -14,11 +14,12 @@ int getRandomNumber(int min, int max) {
 char getUserInput() {
 	char selection;
 
-	cout << "please enter your Choise: " << endl;
-	cin >> selection;
+	do {
+		cout << "Please enter your Choice: " << endl;
+		cin >> selection;
+	} while (selection != 'p' && selection != 's' && selection != 'r');
 
 	return selection;
-
 
 }
 
@@ -38,7 +39,7 @@ char getComputerMove(int min, int max) {
 	}
 }
 
-void gameLogic() {
+void gameLogic(int& winCount, int& lostCount, int& tieCount) {
 	char compMove;
 	char usrMove;
 
@@ -47,37 +48,55 @@ void gameLogic() {
 
 	if (compMove == 'r' && usrMove == 'p') {
 		cout << "You win" << endl;
+		winCount++;
 	}
 	else if (compMove == 'r' && usrMove == 's') {
 		cout << "You lose" << endl;
+		lostCount++;
 	}
 	else if (compMove == 'r' && usrMove == 'r') {
 		cout << "its a tie" << endl;
+		tieCount++;
 	}
 	else if (compMove == 'p' && usrMove == 'p') {
 		cout << "its a tie" << endl;
+		tieCount++;
 	}
 	else if (compMove == 'p' && usrMove == 's') {
 		cout << "you win" << endl;
+		winCount++;
 	}
 	else if (compMove == 'p' && usrMove == 'r') {
 		cout << "You lose" << endl;
+		lostCount++;
 	}
 	else if (compMove == 's' && usrMove == 'p') {
 		cout << "you lose" << endl;
+		lostCount++;
 	}
 	else if (compMove == 's' && usrMove == 's') {
 		cout << "its a tie" << endl;
+		tieCount++;
 	}
 	else if (compMove == 's' && usrMove == 'r') {
 		cout << "you win" << endl;
+		winCount++;
 	}
 
+}
+
+void displayGameCount(int gameCount, int winCount, int lostCount, int tieCount) {
+	cout << "The game count is : " << gameCount << endl;
+	cout << "You have won: " << winCount << " games. " << "You have lost: " << lostCount << " Games. " << "and tied: " << tieCount << " games." << endl;
 }
 
 int main() {
 
 	int a;
+	int gameCount = 0;
+	int winCount = 0;
+	int lostCount = 0;
+	int tieCount = 0;
 
 	do {
 		cout << "Lets play Rock, Paper, Scissor" << endl;
@@ -86,13 +105,14 @@ int main() {
 		cin >> a;
 
 		switch (a) {
-		case 1: gameLogic();
+		case 1: gameLogic(winCount, lostCount, tieCount);
+			gameCount++;
 			break;
-		case 2:;
+		case 2: displayGameCount(gameCount, winCount, lostCount, tieCount);
 			break;
 		}
 
 	} while (a != 5);
 
-
+	return 0;
 };
